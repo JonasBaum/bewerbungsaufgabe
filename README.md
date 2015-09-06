412e3ef67d732ce573a48bd648e8a2c7f42f4014
jonasb@gmx.net <jonasb@gmx.net>
2015-09-06 16:19:10 +0200

    code refactoring, readme, comments

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# bewerbungsaufgabe
+# Bewerbungsaufgabe
+
+Functionality
+------------------
+
+-- change background color and save choice in browser storage
+-- html5 audio player with a choice of 4 ABBA songs, get last played song from browser storage
+-- enable/disable auto play music, is also saved to browser storage
+-- control panel with the storage time and the current values
--- resource/js/script.js
+++ resource/js/script.js
@@ -1,25 +1,40 @@
 /**
- * Created by jonas on 9/6/15.
+ * Created by jonas baumgartner on 9/6/15.
  */
 
+//global variables
 var bgcolorForm = document.getElementById('bgcolor');
 var audioForm = document.getElementById('audioItem');
 var audioPlay = document.getElementById('audioAutoPlay');
 
-if(!localStorage.getItem('bgcolorForm')) {
-    populateStorage();
-} else {
-    setChangedAttr();
+//initialize project
+init();
+
+/**
+ * initialize javascript functions
+ * background-color
+ * audio
+ */
+function init(){
+    if(!localStorage.getItem('bgcolorForm')) {
+        populateStorage();
+    } else {
+        setChangedAttr();
+    }
+
+    bgcolorForm.onchange = populateStorage;
+    audioForm.onchange = populateStorage;
+    audioPlay.onchange = populateStorage;
 }
 
-if (!localStorage.getItem('audioAutoPlay')) {
-    audioPlay.checked = false;
-}
-
+/**
+ * populate storage and update changed values
+ * check if storage is once created
+ */
 function populateStorage() {
     localStorage.setItem('bgcolorForm', document.getElementById('bgcolor').value);
     localStorage.setItem('audioItem', document.getElementById('audioItem').value);
-
+    localStorage.setItem('audioAutoPlay', document.getElementById('audioAutoPlay').checked);
 
     if(!localStorage.getItem('firstDateTime'))
         localStorage.setItem('firstDateTime',  new Date());
@@ -27,38 +42,57 @@ function populateStorage() {
     setChangedAttr();
 }
 
+/**
+ * update values
+ */
 function setChangedAttr() {
+    //initialize variables
     var currentColor = localStorage.getItem('bgcolorForm');
     var currentAudio = localStorage.getItem('audioItem');
     var audioAutoPlay = localStorage.getItem('audioAutoPlay');
-    var firstStoredDateTime = localStorage.getItem('firstDateTime');
     var audio = document.getElementById('audio');
     var oggSource = document.getElementById('oggSource');
     var mp3Source = document.getElementById('mp3Source');
     var backgroundElement = document.getElementById('backgroundColor');
 
+    //set current background color in input field
     document.getElementById('bgcolor').value = currentColor;
+    //set current audio in selection
     document.getElementById('audioItem').value = currentAudio;
+    //set background color as HEX
     backgroundElement.style.backgroundColor = '#' + currentColor;
-
+    // set audio path
     oggSource.src='resource/audio/'+currentAudio+'.ogg';
     mp3Source.src='resource/audio/'+currentAudio+'.mp3';
 
+    //check if auto play is enabled and update checkbox
     if(audioAutoPlay === 'true'){
+        audio.load(); //call this to just preload the audio without playing
         audio.play(); //play audio
+        audioPlay.checked = true;
     }else{
+        audioPlay.checked = false;
         audio.load(); //call this to just preload the audio without playing
     }
 
+    setControlPanel(currentColor,currentAudio,audioAutoPlay)
+}
 
-    document.getElementById('firstStoredDateTime').innerHTML = firstStoredDateTime.toString();
+/**
+ * update control panel values
+ *
+ * @param currentColor
+ * @param currentAudio
+ * @param audioAutoPlay
+ */
+function setControlPanel(currentColor, currentAudio,audioAutoPlay){
+    //set control time
+    document.getElementById('firstStoredDateTime').innerHTML = localStorage.getItem('firstDateTime').toString();
     document.getElementById('updatedDateTime').innerHTML = new Date().toString();
+
+    //set table values
+    document.getElementById('storedValueColor').innerHTML = currentColor;
+    document.getElementById('storedValueSong').innerHTML = currentAudio;
+    document.getElementById('storedValueAutoPlay').innerHTML = audioAutoPlay;
 }
 
-function autoPlay(){
-    localStorage.setItem('audioAutoPlay', document.getElementById('audioAutoPlay').checked);
-}
-
-bgcolorForm.onchange = populateStorage;
-audioForm.onchange = populateStorage;
-audioPlay.onchange = autoPlay;
